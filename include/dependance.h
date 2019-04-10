3e56e8723115655e3d556a89bb48de8c2da78c02
gnouf1 <jeuxforum26@gmail.com>
2019-04-10 10:07:05 +0200

    Add files via upload

---
+++ include/dependance.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include "./navalmap/navalmap.h"
+#include "./navalmap/nm_rect.h"
+#include "./navalmap/navalmap.c"
+#include "./navalmap/nm_rect.c"
